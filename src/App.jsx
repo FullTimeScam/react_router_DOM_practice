02d60d5f385ea89fb2fd50b429ffcedfc420110e
FullTimeScam <418.gingin@gmail.com>
2024-05-14 13:16:58 +0900

    라우팅으로 페이지들 만들기(기본)

--- src/App.jsx
+++ src/App.jsx
@@ -6,10 +6,11 @@ const App = () => {
   return (
     <BrowserRouter>
       {/* 브라우저 라우터로 감싸면 가장 최상위에 적용됨 */}
+      {/* 1. 라우터(BrowserRouter) 불러주고 */}
       <Routes>
-        {/* 폴더링 할거다 = Routes 컴포넌트는 URL 경로에 따라 어떤 컴포넌트를 렌더링할지 정의함 */}
+        {/* 2. 폴더링(Routes) 시작하고 폴더링 할거다 = Routes 컴포넌트는 URL 경로에 따라 어떤 컴포넌트를 렌더링할지 정의함 */}
+        {/* 3. 폴더(Route)를 만든다 */}
         <Route path="/" element={<Home />} />
-        {/* 폴더 */}
         <Route path="/A" element={<A />} />
       </Routes>
     </BrowserRouter>
