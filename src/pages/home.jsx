38f7f5a4db333ab336c2b17acc2ffb5e69b68b62
FullTimeScam <418.gingin@gmail.com>
2024-05-14 13:54:41 +0900

    Home.jsx에 버튼 만들기

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,6 +1,12 @@
 const Home = () => {
   return (
     <div className="bg-red-100 min-h-screen flex justify-center items-center">
+      <nav className="bg-bule-100 fixed top-0 left-0 flex justify-center gap-4">
+        <button>Home</button>
+        <button>a</button>
+        <button>b</button>
+        <button>c</button>
+      </nav>
       Home
     </div>
   );
