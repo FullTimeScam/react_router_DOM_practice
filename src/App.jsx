37a714532bcfa20c160b6196208ffd563f3bddfa
FullTimeScam <418.gingin@gmail.com>
2024-05-14 11:29:38 +0900

    라우터 사용하기(페이지를 여러개로 나누기)

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,19 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/home";
+import A from "./pages/a";
+
 const App = () => {
-  return <div className="bg-red-100">Hello, React!</div>;
+  return (
+    <BrowserRouter>
+      {/* 브라우저 라우터로 감싸면 가장 최상위에 적용됨 */}
+      <Routes>
+        {/* 폴더링 할거다 = Routes 컴포넌트는 URL 경로에 따라 어떤 컴포넌트를 렌더링할지 정의함 */}
+        <Route path="/" element={<Home />} />
+        {/* 폴더 */}
+        <Route path="/A" element={<A />} />
+      </Routes>
+    </BrowserRouter>
+  );
 };
 
 export default App;
---
+++ src/pages/a.jsx
@@ -0,0 +1,9 @@
+const Home = () => {
+  return (
+    <div className="bg-blue-100 min-h-screen flex justify-center items-center">
+      A
+    </div>
+  );
+};
+
+export default Home;
